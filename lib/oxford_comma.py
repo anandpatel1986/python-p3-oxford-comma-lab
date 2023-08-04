e119a421f3be45d1dbf43d3a9bb4de2bc591ab81
Anandkumar Patel <avpatel2005@gmail.com>
2023-08-04 19:47:47 -0400

    complete lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return f"{items[0]}"
+    elif len(items) == 2:
+        return "{} and {}".format(items[0], items[1])
+    else:
+        return "{}, and {}".format(", ".join(items[:-1]), items[-1])
+        
